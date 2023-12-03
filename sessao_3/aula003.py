e787772626e3b3a0ae9a9bce4f7ed2f9f0e63599
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-03 15:26:58 -0300

    Create aula003.py

---
+++ sessao_3/aula003.py
@@ -0,0 +1,19 @@
+print(1234)
+print("Edivaldo Ribeiro")
+print('Edivaldo Ribeiro')
+print("\"Edivaldo\" \"Ribeiro\"")
+print('"Edivaldo" "Ribeiro"')
+print("'Edivaldo' 'Ribeiro'")
+print(r"\"Edivaldo\" \"Ribeiro\"")  #r significa expressao regular
+
+'''
+Saida ao executar:
+
+1234
+Edivaldo Ribeiro
+Edivaldo Ribeiro
+"Edivaldo" "Ribeiro"
+"Edivaldo" "Ribeiro"
+'Edivaldo' 'Ribeiro'
+\"Edivaldo\" \"Ribeiro\"
+'''
