5fc076f6ff9c9408d7e5ef43c69f73b0db7e50c4
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-03 15:33:48 -0300

    Create aula012.py

---
+++ sessao_3/aula012.py
@@ -0,0 +1,13 @@
+nome="Edivaldo Ribeiro"
+peso=83
+altura=1.72
+imc=...                     #isso nao gera erro, é um Ellipsis, ou seja, será atribuido depois
+imc=peso/altura**2
+print("nome:", nome, "imc:", imc)
+
+
+"""
+Exibe ao executar:
+
+nome: Edivaldo Ribeiro imc: 28.055705786911847
+"""
