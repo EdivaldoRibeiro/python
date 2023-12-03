a99eb5ec4c3bd67b932cfc942251914eb8921eed
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-03 18:15:20 -0300

    Create aula018.py

---
+++ sessao_3/aula018.py
@@ -0,0 +1,39 @@
+#aula exercitou o uso de debug do python
+
+condicao1=1
+condicao2=2
+
+resp=False
+if condicao1==1:
+    print("condicao1 do if")
+    resp=True
+else:
+    print("nao atende condicao1 do if")
+
+if condicao2==2:
+    print("condicao2 do if")
+    resp=True
+else:
+    print("Nao atende condicao2 do if")
+
+if resp:
+    if condicao1==1 or condicao2==2:
+        print("condicao1 ou condicao2 do if")
+
+    if condicao1==1 and condicao2==2:
+        print("condicao1 e condicao2 do if")
+else:
+    print("fora de condicao1 e condicao2 do if")
+
+print("fora do if")
+
+
+"""
+Exibe ao executar:
+
+condicao1 do if
+condicao2 do if
+condicao1 ou condicao2 do if
+condicao1 e condicao2 do if
+fora do if
+"""
