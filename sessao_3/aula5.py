a3e490b0705944c7dd7807e9bc8e4db4c11e7ca3
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-03 12:09:10 -0300

    Create aula5.py

---
+++ sessao_3/aula5.py
@@ -0,0 +1,13 @@
+print(10==10, type(10==10)) #true
+print(1==2) #false
+print(type(True))
+print(type(False))
+
+'''
+Saida ao executar:
+
+True <class 'bool'>
+False
+<class 'bool'>
+<class 'bool'>
+'''
