14dca5a9d265752d94f142077ecc7ceadb7281c3
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-03 15:29:07 -0300

    Create aula006.py

---
+++ sessao_3/aula006.py
@@ -0,0 +1,23 @@
+print(1+1)
+print('1'+'1')
+print(int('1')+1)
+print(float('1.0')+1.1)
+print(str(100)+"1")
+print(bool(''), "deve ser false")
+print(bool(' '), "deve ser true")
+print(bool(1), "deve ser true")
+print(bool(0), "deve ser false")
+
+"""
+Saida quando executado:
+
+2
+11
+2
+2.1
+1001
+False deve ser false
+True deve ser true
+True deve ser true
+False deve ser false
+"""
