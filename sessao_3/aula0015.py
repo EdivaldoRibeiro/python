2b7a534cdef50df85454415647f4d08f84fe4c50
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-03 17:24:47 -0300

    Create aula0015.py

---
+++ sessao_3/aula0015.py
@@ -0,0 +1,21 @@
+nome=input("Nome: ")
+formato="Meu nome é: {0}"
+print(formato.format(nome))
+
+val1=input("Digite numero1= ")
+val2=input("Digite numero2= ")
+
+numero1=int(val1)
+numero2=int(val2)
+
+print(f'a soma de {numero1} e {numero2} = {numero1+numero2}')
+
+"""
+Exibe ao executar:
+
+Nome: Edivaldo
+Meu nome é: Edivaldo
+Digite numero1= 1 
+Digite numero2= 5
+a soma de 1 e 5 = 6
+"""
