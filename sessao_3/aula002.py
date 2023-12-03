e9152acda8a9a545cf9fdd8a7ee21e2bb30229b6
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-03 15:26:18 -0300

    Create aula002.py

---
+++ sessao_3/aula002.py
@@ -0,0 +1,13 @@
+#\r\n -> CRLF
+#\n - LF
+print(12,34,sep="->",end="\r\n")
+print(56,78,sep="=>",end="###\n")
+print("Ribeiro","Edivaldo", sep=" ")
+
+"""
+Saida ao executar:
+
+12->34
+56=>78###
+Ribeiro Edivaldo
+"""
