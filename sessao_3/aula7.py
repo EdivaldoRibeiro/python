b0abc110bd84fad8cf76349cd40c28c7f65ddf8d
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-03 14:55:23 -0300

    Create aula7.py

---
+++ sessao_3/aula7.py
@@ -0,0 +1,22 @@
+nome_completo="Edivaldo Ribeiro"
+print(nome_completo)
+nome_completo="EDIVALDO RIBEIRO"
+print(nome_completo)
+valor_qualquer=5
+print(valor_qualquer)
+valor_qualquer="amanha"
+print(valor_qualquer)
+
+saldo=1000000.00
+negativo=saldo<0
+print("saldo:", saldo, "esta negativo:", negativo)
+
+"""
+Saida ao executar:
+
+Edivaldo Ribeiro
+EDIVALDO RIBEIRO
+5
+amanha
+saldo: 1000000.0 esta negativo: False
+"""
