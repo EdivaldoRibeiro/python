54fb54692d62b26060671b84572abd8daf72b335
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-03 15:30:34 -0300

    Create aula008.py

---
+++ sessao_3/aula008.py
@@ -0,0 +1,24 @@
+nome="Edivaldo"
+sobrenome="Ribeiro"
+idade=60
+ano_nascimento=2023-idade
+maior_idade=ano_nascimento>=18
+altura=1.72
+
+print("nome:", nome)
+print("sobrenome:", sobrenome)
+print("idade:", idade)
+print("ano de nascimento:", ano_nascimento)
+print("e maior de idade:", maior_idade)
+print("altura em metros:", altura)
+
+"""
+Exibe ao executar:
+
+nome: Edivaldo
+sobrenome: Ribeiro
+idade: 60
+ano de nascimento: 1963
+e maior de idade: True
+altura em metros: 1.72
+"""
