ea89245239d639dddb34613976cae59cd4beced7
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-03 14:56:59 -0300

    Create aula10.py

---
+++ sessao_3/aula10.py
@@ -0,0 +1,16 @@
+concatenacao="A"+"B"+"C"
+b_cinco_vezes="b"*5
+mane_tres_vezes="mane"*5
+
+print("concatenacao:", concatenacao)
+print("b_cinco_vezes:", b_cinco_vezes)
+print("mane_tres_vezes:", mane_tres_vezes)
+
+
+"""
+Exibe ao executar:
+
+concatenacao: ABC
+b_cinco_vezes: bbbbb
+mane_tres_vezes: manemanemanemanemane
+"""
