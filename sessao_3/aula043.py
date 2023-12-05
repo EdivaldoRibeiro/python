f6e47101b67d038f1bb64a1f724d264f349739d3
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-05 16:36:23 -0300

    Create aula043.py

---
+++ sessao_3/aula043.py
@@ -0,0 +1,29 @@
+nome='Edivaldo Ribeiro'
+novo_nome=''
+for caracter in nome:
+    novo_nome+=f'_{caracter}'
+    print(caracter)
+print(novo_nome)
+
+"""
+Exibe ao executar:
+
+
+E
+d
+i
+v
+a
+l
+d
+o
+ 
+R
+i
+b
+e
+i
+r
+o
+_E_d_i_v_a_l_d_o_ _R_i_b_e_i_r_o
+"""
