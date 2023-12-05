4052c9437491bea2ffb60c6f56dadde65520845d
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-05 17:11:41 -0300

    Create aula044.py

---
+++ sessao_3/aula044.py
@@ -0,0 +1,25 @@
+nome='Edivaldo Ribeiro'
+numeros=range(0,len(nome),1)
+for numero in numeros:
+    print(f'{numero}-{nome[numero]}')
+
+"""
+Exibe ao executar:
+
+0-E
+1-d
+2-i
+3-v
+4-a
+5-l
+6-d
+7-o
+8- 
+9-R
+10-i
+11-b
+12-e
+13-i
+14-r
+15-o
+"""
