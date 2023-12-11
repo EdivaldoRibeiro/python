167d90710b3ab93b5f2b4b1509638c701266d7e1
Edivaldo Ribeiro <45264769+EdivaldoRibeiro@users.noreply.github.com>
2023-12-11 10:45:10 -0300

    Create aula070.py

---
+++ sessao_3/aula070.py
@@ -0,0 +1,14 @@
+def calculo(x,y):
+    retorno=x+y
+    print(f'x={x} y={y} retorno={retorno}')
+    return retorno
+
+calculo(1,2)
+calculo('a', 'b')
+
+"""
+Exibe ao executar:
+
+x=1 y=2 retorno=3
+x=a y=b retorno=ab
+"""
